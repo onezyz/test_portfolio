0d17448d6dcea3c31fa6aefa5970ecaa5ee234e9
onezyz <wei3on@naver.com>
2022-05-06 14:25:54 +0900

    포트폴리오 메인파일 및 css js 코드 수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const test = document.querySelector(".test");
+const test2 = document.querySelector("span");
 
 test.addEventListener("click",function(){
-    alert("경고창");
+    alert("경고창!");
+});
+test.addEventListener("mouseenter",function(){
+    alert("마우스 올림");
 });
